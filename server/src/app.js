16d2649cb9e2139a32b074afb8e70396482200bb
mak <mharrvic@gmail.com>
2017-10-31 11:40:09 +0800

    Updating Express

--- server/src/app.js
+++ server/src/app.js
@@ -1,5 +1,4 @@
 console.log('hello')
-
 const express = require('express')
 const bodyParser = require('body-parser')
 const cors = require('cors')
@@ -10,9 +9,11 @@ app.use(morgan('combined'))
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get('/status', (req, res) => {
+app.post('/register', (req, res) => {
   res.send({
-    message: 'hello world!'
+    message: `Hello ${req.body.email}! Your user was registered!`
   })
 })
 app.listen(process.env.PORT || 8081)
+
+console.log('hello mak')
