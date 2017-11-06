bae528d5bde5768501c9f2f9a1a77d2a7a45ced0
mak <mharrvic@gmail.com>
2017-11-06 15:38:07 +0800

    Save Data

--- server/src/controllers/SongsController.js
+++ server/src/controllers/SongsController.js
@@ -32,5 +32,19 @@ module.exports = {
         error: 'An error has occured trying to create the song'
       })
     }
+  },
+  async put (req, res) {
+    try {
+      const song = await Song.update(req.body, {
+        where: {
+          id: req.params.songId
+        }
+      })
+      res.send(req.body)
+    } catch (err) {
+      res.status(500).send({
+        error: 'an error has occured trying to update the song'
+      })
+    }
   }
 }
--- server/src/routes.js
+++ server/src/routes.js
@@ -14,12 +14,12 @@ module.exports = (app) => {
   app.get('/songs',
     SongsController.index)
 
-  app.post('/songs',
-    SongsController.post)
-
   app.get('/songs/:songId',
     SongsController.show)
 
   app.put('/songs/:songId',
     SongsController.put)
+
+  app.post('/songs',
+    SongsController.post)
 }
